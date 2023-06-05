52324d1fdddfbfe39056350530928c1b427b4a6b
Maksym Khaiuk <maksym.khaiuk@magicpowered.io>
2023-06-05 18:32:37 +0300

    fix: title rename

--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -3,7 +3,6 @@
 import { FC } from 'react';
 import cn from 'classnames';
 
-import useMainContext from '@app/context/mainContext';
 import { ThemeSwitcher } from '@app/components/ThemeSwitcher';
 
 import s from './MainLayout.module.scss';
@@ -14,12 +13,10 @@ export interface MainLayoutProps {
 }
 
 export const MainLayout: FC<MainLayoutProps> = ({ children, className }) => {
-  const { lightTheme } = useMainContext();
-
   return (
     <div className={cn(s.main, className)}>
       <header className={s.header}>
-        Refresh
+        Portfolio Maksym K.
         <ThemeSwitcher />
       </header>
       <main className={s.content}>{children}</main>
