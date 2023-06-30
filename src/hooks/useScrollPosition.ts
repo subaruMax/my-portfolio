8de9daf563f842e03a3d3843094d445ca04d8f82
Maksym Khaiuk <maksym.khaiuk@magicpowered.io>
2023-06-30 16:52:24 +0300

    feat: updated useScrollPosition hook; implement header scroll progress

--- src/hooks/useScrollPosition.ts
+++ src/hooks/useScrollPosition.ts
@@ -4,11 +4,32 @@ import { useEffect, useState } from 'react';
 const DEBOUNCE_TIME = 50;
 
 const useScrollPosition = () => {
-  const [scrollPosition, setScrollPosition] = useState(0);
+  const [scrollPosition, setScrollPosition] = useState({
+    current: 0,
+    total: 0,
+    currentPercent: 0
+  });
 
   useEffect(() => {
     const updatePosition = () => {
-      setScrollPosition(window.scrollY);
+      const documentHeight = Math.max(
+        document.body.scrollHeight,
+        document.body.offsetHeight,
+        document.documentElement.clientHeight,
+        document.documentElement.scrollHeight,
+        document.documentElement.offsetHeight
+      );
+
+      const scrollPosition = window.scrollY;
+      const scrollPercentage = Math.round(
+        (scrollPosition / (documentHeight - window.innerHeight)) * 100
+      );
+
+      setScrollPosition({
+        current: scrollPosition,
+        total: documentHeight,
+        currentPercent: scrollPercentage
+      });
     };
 
     const debounced = debounce(updatePosition, DEBOUNCE_TIME);
